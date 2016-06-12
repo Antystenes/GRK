9898808564d693c35b94b38c5d5f6f2e9741a65f
Morgoth <morgoth@localhost.localdomain>
2016-06-12 13:03:18 +0200

    Added simple fog

--- shaders/Fragment_Shader.glsl
+++ shaders/Fragment_Shader.glsl
@@ -1,61 +1,17 @@
+
 #version 430 core
 
-// Caustics
-// Original code Copyright (c) Nvidia Corporation. All Rights Reserved.
-// The Shader is based on the original work by Daniel Sanchez-Crespo
-// of the Universitat Pompeu Fabra, Barcelona, Spain.
-
-#define VTXSIZE 0.01f  // Amplitude
-#define WAVESIZE 10.0f // Frequency
-#define FACTOR 1.0f
-#define SPEED 2.0f
-#define OCTAVES 5
-
-out vec4 color;
-in vec4 Position;
-uniform sampler2D LightMap;
-uniform sampler2D GroundMap;
-in uniform float Timer;
+out vec3 color;
 in vec2 texcoord;
-
-// Derivative of wave function. Returns df/dx and df/dy partial derivatives
-vec2 gradwave(float x, float y, float timer)
-{
-    float dZx = 0.0f;
-    float dZy = 0.0f;
-    float octaves = OCTAVES;
-    float factor = FACTOR;
-    float d = sqrt(x*x + y*y); //distance
-    float inside;
-
-    do
-    {
-        inside = timer * SPEED + (1/factor) * x * y * WAVESIZE;
-        dZx += d * sin(inside) * y * WAVESIZE - factor * cos(inside) * x/d;
-        dZy += d * sin(inside) * x * WAVESIZE - factor * cos(inside) * y/d;
-        factor = factor / 2;
-        octaves--;
-    } while (octaves > 0);
-
-    return vec2(2 * VTXSIZE * dZx, 2 * VTXSIZE * dZy);
-}
-
-vec3 line_plane_intercept(vec3 lineP, vec3 lineN, vec3 planeN, float planeD)
-{
-    // This implementation assumes that planeN is always pointing up
-    float dst = (planeD - lineP.z) /lineN.z; //distance
-    return lineP + lineN * dst;
-}
+in vec2 noise;
+in float f;
+uniform sampler2D text;
+//in vec4 vNormal;
+//in vec4 vL;
+const vec3 fog = vec3(0.0, 0.0, 0.5);
 
 void main(void)
 {
-    vec2 dxdy = gradwave(Position.x, Position.y, Timer);
-
-    vec3 intercept = line_plane_intercept(Position.xyz, vec3(dxdy, clamp(Position.w, 0.0, 1.0)), vec3(0,0,1), -0.8);
-
-    color.rgb = vec3(texture(LightMap, intercept.xy * 0.8));
-    color.rgb += vec3(texture(GroundMap, Position.xy));
-    color.a = 1;
-
-    //color = texture(GroundMap, texcoord).rgba;
+color = f * texture(text, texcoord).rgb + (1-f) * fog;
+// * max (dot(vNormal, vL),0);
 }
--- shaders/Vertex_Shader.glsl
+++ shaders/Vertex_Shader.glsl
@@ -7,9 +7,12 @@ uniform mat4 transformation;
 uniform mat4 camera;
 uniform mat4 perspective;
 out vec2 texcoord;
+out float f;
 
 void main(void)
 {
+vec4 pos = camera * transformation * vec4(position, 1);
+f = exp(-0.3*length(pos));
 texcoord = tex;
-gl_Position =  perspective * camera * transformation * vec4(position, 1);
+gl_Position =  perspective * pos;
 }
